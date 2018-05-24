94452acb38e1369988d00485ed8b7db10742c196
yaochao <yaochao365@qq.com>
2018-05-24 15:23:36 +0800

    add the Table Model

--- table/models.py
+++ table/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Table(models.Model):
+    tab_name = models.CharField(max_length=64) # '表名'
+    tab_comment = models.CharField(max_length=1024) # '表说明'
+    tab_ver = models.IntegerField() # '版本号，系统自动记录'
+    tab_buzz = models.SmallIntegerField() # '业务类型， 0 字典类，1 居民健康档案 2慢病  3 老年人 4 妇幼、儿童 5 家庭医生签约 9 其他'
+    tab_type = models.SmallIntegerField(default=1) # '类型， 1表 TABLE， 2 视图 VIEW'
+    tab_status = models.SmallIntegerField(default=1) # '表状态， 默认为1 启用， 0为不使用'
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.tab_name
+
+    class Meta:
+        ordering = ['tab_name']
--- table/views.py
+++ table/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from table.models import Table
 
-# Create your views here.
+
+def home(request):
+    tables = Table.objects.all()
+    return HttpResponse('hello world! this is my home.')
